608ffe1a61aa06e524780f86016a540db1f07d58
SimanyeSTS <simanyesomdaka@gmail.com>
2024-05-14 12:51:45 +0200

    Java

---
+++ script/index.js
@@ -0,0 +1,31 @@
+
+    const friends = [];
+
+    const firstNameInput = document.getElementById('firstName');
+    const lastNameInput = document.getElementById('lastName');
+    const addFriendBtn = document.getElementById('addFriendBtn');
+    const clearBtn = document.getElementById('clearBtn');
+
+    addFriendBtn.addEventListener('click', () => {
+        const firstName = firstNameInput.value.trim();
+        const lastName = lastNameInput.value.trim();
+
+        if (firstName && lastName) {
+            friends.push({ firstName, lastName });
+            console.log('Friend added:', { firstName, lastName });
+            console.log('Friends array:', friends);
+
+            // Clear input fields
+            firstNameInput.value = '';
+            lastNameInput.value = '';
+        } else {
+            alert('Please enter both first name and last name.');
+        }
+    });
+
+    clearBtn.addEventListener('click', () => {
+        while (friends.length > 0) {
+            const friend = friends.pop();
+            console.log('Removed friend:', friend);
+        }
+    });
