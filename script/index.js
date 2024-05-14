8162b7b0b82b8adfe8a809e9cdd27ccddec735c2
SimanyeSTS <simanyesomdaka@gmail.com>
2024-05-14 16:05:01 +0200

    16:04

--- script/index.js
+++ script/index.js
@@ -1,31 +1,43 @@
 
-    const friends = [];
+    // const friends = [];
 
-    const firstNameInput = document.getElementById('firstName');
-    const lastNameInput = document.getElementById('lastName');
-    const addFriendBtn = document.getElementById('addFriendBtn');
-    const clearBtn = document.getElementById('clearBtn');
+    // const firstNameInput = document.getElementById('firstName');
+    // const lastNameInput = document.getElementById('lastName');
+    // const addFriendBtn = document.getElementById('addFriendBtn');
+    // const clearBtn = document.getElementById('clearBtn');
 
-    addFriendBtn.addEventListener('click', () => {
-        const firstName = firstNameInput.value.trim();
-        const lastName = lastNameInput.value.trim();
+    // addFriendBtn.addEventListener('click', () => {
+    //     const firstName = firstNameInput.value.trim();
+    //     const lastName = lastNameInput.value.trim();
 
-        if (firstName && lastName) {
-            friends.push({ firstName, lastName });
-            console.log('Friend added:', { firstName, lastName });
-            console.log('Friends array:', friends);
+    //     if (firstName && lastName) {
+    //         friends.push({ firstName, lastName });
+    //         console.log('Friend added:', { firstName, lastName });
+    //         console.log('Friends array:', friends);
 
-            // Clear input fields
-            firstNameInput.value = '';
-            lastNameInput.value = '';
-        } else {
-            alert('Please enter both first name and last name.');
-        }
-    });
+    //         // Clear input fields
+    //         firstNameInput.value = '';
+    //         lastNameInput.value = '';
+    //     } else {
+    //         alert('Please enter both first name and last name.');
+    //     }
+    // });
 
-    clearBtn.addEventListener('click', () => {
-        while (friends.length > 0) {
-            const friend = friends.pop();
-            console.log('Removed friend:', friend);
-        }
-    });
+    // clearBtn.addEventListener('click', () => {
+    //     while (friends.length > 0) {
+    //         const friend = friends.pop();
+    //         console.log('Removed friend:', friend);
+    //     }
+    // });
+
+
+//Joel Example:
+
+//Empty Erray
+let friends = []
+let addFriends = document.getElementById('[addFriendBtn]')
+addFriends.addEventListener('click', () => {
+    friends.push({
+      friends  
+    })
+})
